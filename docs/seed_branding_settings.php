136a95ba6580da4d06dcc1cb552025ec1877f68d
Muzikayise Khuzwayo <khuzwayomuzikayise@gmail.com>
2026-08-23 06:32:05 +0200

    Force MySQL connection in seed_branding_settings.php

--- docs/seed_branding_settings.php
+++ docs/seed_branding_settings.php
@@ -1,6 +1,19 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+// Set production DB env before bootstrapping Testbench
+$_ENV['DB_CONNECTION'] = 'mysql';
+$_ENV['DB_HOST'] = '127.0.0.1';
+$_ENV['DB_PORT'] = '3306';
+$_ENV['DB_DATABASE'] = 'laravel_crm';
+$_ENV['DB_USERNAME'] = 'crm_user';
+$_ENV['DB_PASSWORD'] = 'SecurePass123!';
+
+putenv('DB_CONNECTION=mysql');
+putenv('DB_HOST=127.0.0.1');
+putenv('DB_PORT=3306');
+putenv('DB_DATABASE=laravel_crm');
+putenv('DB_USERNAME=crm_user');
+putenv('DB_PASSWORD=SecurePass123!');
 
 $app = require_once __DIR__ . '/../vendor/orchestra/testbench-core/laravel/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
